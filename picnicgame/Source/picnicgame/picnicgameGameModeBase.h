1e0de565963527f356474da87b2a3852e96f2925
andihegedus <74625948+andihegedus@users.noreply.github.com>
2025-03-19 15:15:54 -0700

    Add unreal project files

---
+++ picnicgame/Source/picnicgame/picnicgameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "picnicgameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PICNICGAME_API ApicnicgameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
